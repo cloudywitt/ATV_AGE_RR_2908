d3568da6a493e273cec62878154b8de87049690d
Edu456 <104273975+Edu456@users.noreply.github.com>
2023-08-29 21:49:22 -0300

    Update questaoE.s

---
+++ Questao02/E/questaoE.s
@@ -0,0 +1,21 @@
+soma: 0.0
+msg: "soma:"
+
+li t1, 0
+li t1, 30
+
+bge t0, t1, exit
+li t0, 1.0
+li t2, 3.0
+div t0, t0, t2
+li t2, soma2
+
+add t2, t2, t0
+s t2, soma2
+
+addi t0,t0,1
+j loop
+
+li t5, soma2
+li v0, 2
+syscall
