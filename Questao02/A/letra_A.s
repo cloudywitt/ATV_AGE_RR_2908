d5d13bc248bc3e385802c87adf20834f91e41289
Edu456 <104273975+Edu456@users.noreply.github.com>
2023-08-29 22:09:04 -0300

    Update letra_A.s

--- Questao02/A/letra_A.s
+++ Questao02/A/letra_A.s
@@ -1,12 +1,12 @@
-li s1, 12
-li s2, 5
+li $s1, 12
+li $s2, 5
 
-add t0, s1, s1
-add t1, s1, s2
-sub s3,t0, t1
+add $t0, $s1, $s1
+add $t1, $s1, $s2
+sub $s3, $t0, t1
 
-sw t4, t
+sw $t4, t
 
-li v0, 1
-li a0, t
+li $v0, 1
+li $a0, t
 sycall
